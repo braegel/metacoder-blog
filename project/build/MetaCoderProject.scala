1700f1e559c9b7f994ae93beaad5f916d16a1e63
Benjamin Neff & Felix Becker (pair programming) <github@felixbecker.name, benjamin@coding4coffee.ch>
2011-06-11 04:01:59 +0200

    alte projekt-datei geloescht

--- project/build/MetaCoderProject.scala
+++
@@ -1,8 +0,0 @@
-import sbt._
-
-class MetaCoderProject(info : ProjectInfo) extends DefaultWebProject(info) {
-  val log4j = "log4j" % "log4j" % "1.2.16"
-
-  val jetty7 = "org.eclipse.jetty" % "jetty-webapp" % "7.1.6.v20100715" % "test"
-  val servletApi = "javax.servlet" % "servlet-api" % "2.5" % "provided"
-}
